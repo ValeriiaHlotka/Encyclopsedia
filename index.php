1ce4cb979c2f21993aabeaa8119847f12d94f880
Lera <valeriya.glotka@webhome.company>
2021-11-30 14:09:40 +0200

    index to prestart

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 ini_set('display_errors', 1);
-//require_once "app/prestart.php";
-require_once 'entertainment/AR/test.php';
+require_once "app/prestart.php";
+//require_once 'entertainment/AR/test.php';
