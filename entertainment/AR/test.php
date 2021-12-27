ace8ff508f9c1ef08ceb14ebe9635e60c6a25cc3
Lera <valeriya.glotka@webhome.company>
2021-12-27 19:48:24 +0200

    ar try 13

--- entertainment/AR/test.php
+++ entertainment/AR/test.php
@@ -102,7 +102,7 @@
         <a-entity
                 position="0 -1 0"
                 scale="0.05 0.05 0.05"
-                gltf-model="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/scene.gltf"
+                gltf-model="entertainment/AR/burger.glb"
         ></a-entity>
     </a-marker-camera>
     <a-entity camera></a-entity>
