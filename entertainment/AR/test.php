b1498d612bf0b0c7a01937d6efef8f94f24e4e6e
Lera <valeriya.glotka@webhome.company>
2021-12-27 20:21:20 +0200

    ar try 16

--- entertainment/AR/test.php
+++ entertainment/AR/test.php
@@ -97,15 +97,15 @@
 <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
 <body style="margin : 0px; overflow: hidden;">
 <a-scene embedded arjs>
-    <a-marker-camera type="pattern" url="entertainment/AR/ball.patt">
+    <a-marker type="pattern" url="entertainment/AR/ball.patt">
         <!-- we use cors proxy to avoid cross-origin problems ATTENTION! you need to set up your server -->
         <a-entity
                 position="0 -1 0"
                 scale="0.05 0.05 0.05"
                 gltf-model="entertainment/AR/banana.glb"
         ></a-entity>
-    </a-marker-camera>
-    <!--<a-entity camera></a-entity>-->
+    </a-marker>
+    <a-entity camera></a-entity>
 </a-scene>
 </body>
 </html>
