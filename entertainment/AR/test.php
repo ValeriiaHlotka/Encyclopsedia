7f01553ebffc19482086b4af6bb0af347da2b369
Lera <valeriya.glotka@webhome.company>
2021-12-27 20:43:21 +0200

    ar try 17

--- entertainment/AR/test.php
+++ entertainment/AR/test.php
@@ -100,7 +100,7 @@
     <a-marker type="pattern" url="entertainment/AR/ball.patt">
         <!-- we use cors proxy to avoid cross-origin problems ATTENTION! you need to set up your server -->
         <a-entity
-                position="0 2 -2"
+                position="0 0 -2"
                 scale="2 2 2"
                 gltf-model="entertainment/AR/banana.glb"
                 rotation="0 45 45"
