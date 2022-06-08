9eab33a0a879952abaf6ac107974242406e06f42
Lera <valeriya.glotka@webhome.company>
2022-06-09 01:12:59 +0300

    files

--- entertainments/show/banana.php
+++ entertainments/show/banana.php
@@ -4,11 +4,11 @@
 <script src="https://raw.githack.com/AR-js-org/AR.js/master/aframe/build/aframe-ar.js"></script>
 <body style="margin : 0px; overflow: hidden;">
 <a-scene embedded arjs>
-    <a-marker type="pattern" url="entertainment/AR/markers/banana.patt">
+    <a-marker type="pattern" url="/entertainment/AR/markers/banana.patt">
         <a-entity
             position="0 -1 0"
             scale="0.05 0.05 0.05"
-            gltf-model="entertainment/AR/models/banana.glb"
+            gltf-model="/entertainment/AR/models/banana.glb"
             rotation="0 90 45"
         ></a-entity>
     </a-marker>
